8ccc7ca569c5709feda3cd137ed290725f64a040
Joe Connolly <jconnolly@users-MacBook-Pro-2.local>
2019-01-17 01:21:47 -0500

    Got CRUD functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import './App.css';
-
+import uuid from 'uuid';
+import $ from 'jquery';
 
 
 class App extends Component {
@@ -10,33 +11,79 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: []
+      projects: [],
+      todos: []
     }
   }
 
   componentWillMount() {
-      this.setState({projects: [
-        {
-        title: "Business Website",
-        category: "Web design"
-      },
-      {
-        title: "Social App",
-        category: "Mobile Development"
-      },
-      {
-        title: "eCommerche Shopping Cart",
-        category: "Web development"
-      }
-    ]});
+    this.getProjects()
+    this.getTodos();
   }
 
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getProjects() {
+    this.setState({projects: [
+      {
+      id: uuid.v4(),
+      title: "Business Website",
+      category: "Web design"
+    },
+    {
+      id: uuid.v4(),
+      title: "Social App",
+      category: "Mobile Development"
+    },
+    {
+      id: uuid.v4(),
+      title: "eCommerche Shopping Cart",
+      category: "Web development"
+    }
+  ]});
+  }
+
+  getTodos() {
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({todos:data}, function() {
+          console.log('state');
+          console.log(this.state);
+        })
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }
+
+    })
+  }
+
+
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
   render() {
     return (
       <div className="App">
-        <AddProject/>
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects test="Hello World"
-        projects={this.state.projects}/>
+        projects={this.state.projects}
+        onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 
 class ProjectItem extends Component {
+
+  deleteProject(id){
+    // console.log("delete");
+    this.props.onDelete(id);
+  }
+
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <li className="Project">
-      <strong>  {this.props.project.title} </strong> : {this.props.project.title}
+      <strong>  {this.props.project.title} </strong> : {this.props.project.category}
+      <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
+
       </li>
     );
   }
